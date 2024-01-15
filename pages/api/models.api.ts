f521dcff56b8efab011682ab143bb26904d312b3
Oleksii Kulyk <oleksii.kulyk@tomtom.com>
2024-01-15 17:42:49 +0100

    bypass dynamic model

--- pages/api/models.api.ts
+++ pages/api/models.api.ts
@@ -1,63 +1,27 @@
 import {OpenAIModel, OpenAIModelID, OpenAIModels} from "@/types/openai"
-import {OPENAI_API_HOST, OPENAI_API_TYPE, OPENAI_API_VERSION, OPENAI_ORGANIZATION} from "@/utils/app/const"
 
 export const config = {
   runtime: "edge"
 }
 
+const STATIC_MODELS = ["gpt-35-turbo", "gpt-35-turbo-16k", "gpt-4", "gpt-4-32k"]
 const handler = async (req: Request): Promise<Response> => {
-  try {
-    const {apiKey} = (await req.json()) as {apiKey: string}
-
-    let url = `${OPENAI_API_HOST}/v1/models`
-    if (OPENAI_API_TYPE === "azure") {
-      url = `${OPENAI_API_HOST}/openai/models?api-version=${OPENAI_API_VERSION}`
-    }
-    console.debug(`Get models (${OPENAI_API_TYPE}): ${url}`)
-    const headers = {
-      "Content-Type": "application/json",
-      ...(OPENAI_API_TYPE === "openai" && {
-        Authorization: `Bearer ${apiKey || process.env.OPENAI_API_KEY}`
-      }),
-      ...(OPENAI_API_TYPE === "openai" &&
-        OPENAI_ORGANIZATION && {
-          "OpenAI-Organization": OPENAI_ORGANIZATION
-        }),
-      ...(OPENAI_API_TYPE === "azure" && {
-        "api-key": `${apiKey || process.env.OPENAI_API_KEY}`
-      })
-    }
-    const response = await fetch(url, {headers: headers})
-    if (!response.ok) {
-      console.error(`${OPENAI_API_TYPE} returned an error, status:${response.status}`)
-      return new Response(response.body, {
-        status: response.status,
-        headers: response.headers
-      })
-    }
-
-    const json = await response.json()
-    const models: OpenAIModel[] = json.data
-      .map((model: any) => {
-        for (const [key, value] of Object.entries(OpenAIModelID)) {
-          if (value === model.id) {
-            return {
-              id: model.id,
-              name: OpenAIModels[value].name,
-              tokenLimit: OpenAIModels[value].tokenLimit
-            }
-          }
+  const models = STATIC_MODELS.map((model) => {
+    for (const [key, value] of Object.entries(OpenAIModelID)) {
+      if (value === model) {
+        return {
+          id: model,
+          name: OpenAIModels[value].name,
+          tokenLimit: OpenAIModels[value].tokenLimit
         }
-      })
-      .filter(Boolean)
-      .filter((obj: any, index: any, self: any) => {
-        return index === self.findIndex((other: any) => other.id === obj.id)
-      })
-    return new Response(JSON.stringify(models), {status: 200})
-  } catch (error) {
-    console.error(`Error retrieving models, error:${error}`)
-    return new Response("Error", {status: 500, statusText: error ? JSON.stringify(error) : ""})
-  }
+      }
+    }
+  })
+    .filter(Boolean)
+    .filter((obj: any, index: any, self: any) => {
+      return index === self.findIndex((other: any) => other.id === obj.id)
+    })
+  return new Response(JSON.stringify(models), {status: 200})
 }
 
 export default handler
